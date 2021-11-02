d2667a1c136eb34d1c43202a2175e5e92e1575f0
sum16 <godasper@gmail.com>
2021-11-02 10:51:52 +0900

    sample11ni

--- sample2.js
+++ sample2.js
@@ -3,3 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('result_text').textContent = '<a href="https://www.google.com/?hl=ja">google</a>' // 文字列として入る
   document.getElementById('result_html').innerHTML = '<a href="https://www.google.com/?hl=ja">google</a>' // aタグとして生成される
 }, false);
+  
+  // htmlを埋め込むのでなければtextContentを使用する → 高速でセキュリティ上の問題も発生しにくい
