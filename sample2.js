135ab8f7ae158af4dc899723caec58876b74e3c6
sum16 <godasper@gmail.com>
2021-11-02 11:10:38 +0900

    入力ボックス/セレクトボックスの値を取得する

--- sample2.js
+++ sample2.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
   
   // htmlを埋め込むのでなければtextContentを使用する → 高速でセキュリティ上の問題も発生しにくい
+
+
+// 送信ボタンを押すと、入力ボックス/セレクトボックスの値を取得する
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    const name = document.getElementById('name'); // inputのidを取得しvalueで表示
+    console.log(name.value);
+  })
+}, false);
